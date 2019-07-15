c3ffde79189f5bc407678e325ad86499f3e70374
Meltem <melltemkilic@gmail.com>
2019-07-15 08:17:07 -0400

    One step closer to the final

--- components/Credentials.js
+++ components/Credentials.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
-export const Credentials = () => (
+export const Credentials = () => {
+  const linkStyle= {
+    color: 'grey',
+    textDecoration: 'none'
+  }
+
+  return (
     <div style={{ lineHeight: '10px' }}>
-        <p style={{ color: 'rgb(78, 78, 78)', fontWeight: 'bold'}}>Meltem Kilic</p>
-        <p style={{ color: 'grey' }}>@trilingualmel</p>
+      <p style={{ color: 'rgb(78, 78, 78)', fontWeight: 'bold' }}>Meltem Kilic</p>
+      <a href="https://twitter.com/trilingualmel" target="_blank" style={linkStyle}>@trilingualmel</a>
+      {/* {secondPage ? <p>Software Developer</p> : null} */}
     </div>
-)
+  )
+}

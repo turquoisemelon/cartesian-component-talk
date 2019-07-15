dcace0ab81efb5d24ad541d5491f7dbcfa5efdab
Meltem <melltemkilic@gmail.com>
2019-07-15 08:17:07 -0400

    One step closer to the final

--- theme.js
+++ theme.js
@@ -3,14 +3,19 @@ import theme from 'mdx-deck/themes'
 export default {
   ...theme,
   h1: {
-    color: '#c8300d'
+    color: '#c8300d',
+    margin: '18px 0'
   },
   h2: {
-    color: '#c8300d'
+    color: '#c8300d',
+    margin: '18px 0'
   },
   h3: {
     color: '#c8300d'
   },
+  h5: {
+    margin: '8px 0'
+  }
 }
 
 // rangle red: #D34626
